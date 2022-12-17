531f6f367b23ae9fc76059dc40fb4bd316cd2364
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2022-12-18 05:01:30 +0800

    Add remember context

--- src/commands/chat.js
+++ src/commands/chat.js
@@ -1,17 +1,29 @@
+import dotenv from 'dotenv';
 import chatGPT from '../chatGPT/index.js';
 
+dotenv.config();
+
+
+/**
+ * Default values
+ * MAX_REPLY_COUNT = 5
+ * MAX_TEXT_LENGTH = 1000
+ */
+const MAX_REPLY_COUNT = typeof (process.env.MAX_REPLY_COUNT) === 'undefined' ? 5 : Number(process.env.MAX_REPLY_COUNT);
+const MAX_TEXT_LENGTH = typeof (process.env.MAX_TEXT_LENGTH) === 'undefined' ? 1000 : Number(process.env.MAX_TEXT_LENGTH);
 
 const GET_PROBLEMS = 'The service is experiencing some problems, please try again.';
+
 
 export default {
     name: 'chat',
     aliases: ['c'],
     method: 'chat <message>',
-    description: 'chat with chatAI',
+    description: 'chat with AI',
     options: [
         {
             name: "message",
-            description: "message",
+            description: "content",
             type: 3,
             required: true
         }
@@ -22,10 +34,58 @@ export default {
         if (!args[0] || args[0] === '')
             return message.reply({ content: `❌ | Please enter valid message.`, allowedMentions: { repliedUser: false } });
 
+        if (args.join().length > MAX_TEXT_LENGTH)
+            return message.reply({ content: `❌ | Message length exceed ${MAX_TEXT_LENGTH}.`, allowedMentions: { repliedUser: false } });
+
+
+        const prefixString = client.config.prefix + this.aliases;
+        let requestMessage = '';
+        let hasReply = message.reference || null;
+
+        // If you send reply message, it will search for all reply messages on that reply
+        if (hasReply !== null) {
+            try {
+                let cacheReply = [];
+                let currentMsg = message;
+
+                // Find the previous MAX_REPLY_COUNT reply messages
+                let count = 0;
+                while (hasReply !== null && count < MAX_REPLY_COUNT) {
+                    let relpyMsg = await currentMsg.fetchReference();
+                    cacheReply.push(relpyMsg);
+
+                    currentMsg = relpyMsg;
+                    hasReply = currentMsg.reference || null;
+                    count++;
+                }
+
+                // filter reply content prefix and doesn't start with the prefix content
+                let filterReply = cacheReply.reverse()
+                    .filter((msg) => msg.author.bot !== true && msg.content.startsWith(`${prefixString}`) && msg.content !== message.content)
+                    .map((msg) => String(msg.content.split(' ').splice(1)).replaceAll(',', ' '));
+
+                // count total numbers of char, if > MAX_TEXT_LENGTH discard first reply message
+                let textLength = filterReply.reduce((acc, str) => acc + str.length, 0);
+                while (textLength > MAX_TEXT_LENGTH) {
+                    filterReply.shift();
+                    textLength = filterReply.reduce((acc, str) => acc + str.length, 0);
+                }
+
+                // Add filtered reply messages to requestMessage
+                requestMessage += filterReply.join('\n') + '\n';
+            }
+            catch (error) {
+                console.log('ChatGPT: Search reply error');
+                console.log(error);
+            }
+        }
+
+
+        // Add current message content to requestMessage
+        requestMessage += args.join(' ');
 
         try {
-            message.channel.sendTyping();
-            let result = await chatGPT(args.join(' '));
+            let result = await chatGPT(requestMessage);
 
             if (result === '') result = GET_PROBLEMS; // Discord can't send empty message
 
@@ -35,17 +95,22 @@ export default {
             return message.reply({ content: result, allowedMentions: { repliedUser: false } });
         }
         catch (error) {
+            console.log(`ChatGPT: Response error.`);
             console.log(error);
             let result = GET_PROBLEMS;
             return message.reply({ content: result, allowedMentions: { repliedUser: false } });
         }
-
     },
 
     async slashExecute(client, interaction) {
+        let requestMessage = interaction.options.getString("message");
+
+        if (requestMessage.length > MAX_TEXT_LENGTH)
+            return interaction.editReply({ content: `❌ | Message length exceed ${MAX_TEXT_LENGTH}.`, allowedMentions: { repliedUser: false } });
+
 
         try {
-            let result = await chatGPT(interaction.options.getString("message"));
+            let result = await chatGPT(requestMessage);
 
             if (result === '') result = GET_PROBLEMS;
 
@@ -55,6 +120,7 @@ export default {
             return interaction.editReply({ content: result, allowedMentions: { repliedUser: false } });
         }
         catch (error) {
+            console.log(`ChatGPT: Response error.`);
             console.log(error);
             let result = GET_PROBLEMS;
             return interaction.editReply({ content: result, allowedMentions: { repliedUser: false } });
