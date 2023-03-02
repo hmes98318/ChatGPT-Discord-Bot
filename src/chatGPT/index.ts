229f90733a3f99eb4d258f8a055884e8dfee5be5
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-03-02 17:13:49 +0800

    Add GPT3.5 support

--- src/chatGPT/index.ts
+++ src/chatGPT/index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-import { Configuration, OpenAIApi } from 'openai';
+import { ChatCompletionRequestMessageRoleEnum, Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
@@ -9,26 +9,42 @@ const openai = new OpenAIApi(configuration);
 
 /**
  * Default values
- * OPENAI_API_MODEL = 'text-davinci-003'
+ * OPENAI_API_MODEL = 'gpt-3.5-turbo'
+ * OPENAI_API_USER = 'user'
  * OPENAI_API_MAX_TOKEN = 1000
  * OPENAI_TIMEOUT = 30
  */
-const OPENAI_API_MODEL = typeof (process.env.OPENAI_API_MODEL) === 'undefined' ? 'text-davinci-003' : String(process.env.OPENAI_API_MODEL);
+const OPENAI_API_MODEL = typeof (process.env.OPENAI_API_MODEL) === 'undefined' ? 'gpt-3.5-turbo' : String(process.env.OPENAI_API_MODEL);
+const OPENAI_API_USER = typeof (process.env.OPENAI_API_USER) === 'undefined' ? 'user' : String(process.env.OPENAI_API_USER);
 const OPENAI_API_MAX_TOKEN = typeof (process.env.OPENAI_API_MAX_TOKEN) === 'undefined' ? 1024 : Number(process.env.OPENAI_API_MAX_TOKEN);
 const OPENAI_TIMEOUT = typeof (process.env.OPENAI_TIMEOUT) === 'undefined' ? 30 : Number(process.env.OPENAI_TIMEOUT);
 
 
-const chatGPT = async (message: string): Promise<string> => {
+const chatGPT = async (message: string): Promise<any> => {
 
-    const response = await openai.createCompletion({
-        model: OPENAI_API_MODEL,
-        prompt: message,
-        max_tokens: OPENAI_API_MAX_TOKEN
-    },
-        { timeout: OPENAI_TIMEOUT * 1000 }
-    );
+    if (OPENAI_API_MODEL === 'gpt-3.5-turbo' || OPENAI_API_MODEL === 'gpt-3.5-turbo-0301') {
+        const response = await openai.createChatCompletion({
+            model: OPENAI_API_MODEL,
+            messages: [{ role: (OPENAI_API_USER as ChatCompletionRequestMessageRoleEnum), content: message }],
+            max_tokens: OPENAI_API_MAX_TOKEN
+        },
+            { timeout: OPENAI_TIMEOUT * 1000 }
+        );
 
-    const result = String(response.data.choices[0].text);
-    return result;
+        const result = String(response.data.choices[0].message?.content);
+        return result;
+    }
+    else {
+        const response = await openai.createCompletion({
+            model: OPENAI_API_MODEL,
+            prompt: message,
+            max_tokens: OPENAI_API_MAX_TOKEN
+        },
+            { timeout: OPENAI_TIMEOUT * 1000 }
+        );
+
+        const result = String(response.data.choices[0].text);
+        return result;
+    }
 }
 export default chatGPT;
