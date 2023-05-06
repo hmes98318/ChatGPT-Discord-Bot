9b098f8c372feb582a2af1029723a890b25dd1bb
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2023-05-06 23:54:11 +0800

    Add image generate method

---
+++ src/commands/image.ts
@@ -0,0 +1,69 @@
+import * as dotenv from 'dotenv';
+import imageGenerator from '../openai/imageGenerator';
+
+import { Client, Message, CommandInteraction } from "discord.js";
+
+dotenv.config();
+
+
+/**
+ * --- Default values ---
+ * BOT_MAX_TEXT_LENGTH = 1000
+ */
+const BOT_MAX_TEXT_LENGTH = typeof (process.env.BOT_MAX_TEXT_LENGTH) === 'undefined' ? 1000 : Number(process.env.BOT_MAX_TEXT_LENGTH);
+
+const GET_PROBLEMS = 'The service is experiencing some problems, please try again.';
+
+
+export const name = 'image';
+export const aliases = ['img'];
+export const showHelp = true;
+export const method = 'img <prompts>';
+export const description = 'Generate AI image';
+export const options = [
+    {
+        name: "image",
+        description: "content",
+        type: 3,
+        required: true
+    }
+];
+
+export const execute = async (client: Client, message: Message, args: string[]) => {
+    if (!args[0] || args[0] === '')
+        return message.reply({ content: `❌ | Please enter valid message.`, allowedMentions: { repliedUser: false } });
+
+    if (args.join().length > BOT_MAX_TEXT_LENGTH)
+        return message.reply({ content: `❌ | Message length exceed ${BOT_MAX_TEXT_LENGTH}.`, allowedMentions: { repliedUser: false } });
+
+
+    try {
+        let result = await imageGenerator(args.join(' '));
+        return message.reply({ content: result, allowedMentions: { repliedUser: false } });
+    }
+    catch (error) {
+        console.log(`Image generator: Response error.`);
+        console.log(error);
+        let result = GET_PROBLEMS;
+        return message.reply({ content: result, allowedMentions: { repliedUser: false } });
+    }
+}
+
+export const slashExecute = async (client: Client, interaction: CommandInteraction) => {
+    let requestMessage = String(interaction.options.get("image", true));
+
+    if (requestMessage.length > BOT_MAX_TEXT_LENGTH)
+        return interaction.editReply({ content: `❌ | Message length exceed ${BOT_MAX_TEXT_LENGTH}.`, allowedMentions: { repliedUser: false } });
+
+
+    try {
+        let result = await imageGenerator(requestMessage);
+        return interaction.editReply({ content: result, allowedMentions: { repliedUser: false } });
+    }
+    catch (error) {
+        console.log(`Image generator: Response error.`);
+        console.log(error);
+        let result = GET_PROBLEMS;
+        return interaction.editReply({ content: result, allowedMentions: { repliedUser: false } });
+    }
+}
---
+++ src/openai/imageGenerator.ts
@@ -0,0 +1,43 @@
+import * as dotenv from 'dotenv';
+import {
+    Configuration, OpenAIApi,
+    CreateImageRequestSizeEnum, CreateImageRequestResponseFormatEnum, ChatCompletionRequestMessageRoleEnum
+} from 'openai';
+
+dotenv.config();
+
+
+const configuration = new Configuration({ apiKey: String(process.env.OPENAI_API_TOKEN) });
+const openai = new OpenAIApi(configuration);
+
+/**
+ * --- Default values ---
+ * OPENAI_API_IMAGE_SIZE = '1024x1024'
+ * OPENAI_API_IMAGE_RESPONSE_FORMAT = 'url'
+ * OPENAI_API_USER = 'user'
+ * OPENAI_API_TIMEOUT = 30
+ */
+const OPENAI_API_IMAGE_SIZE = typeof (process.env.OPENAI_API_IMAGE_SIZE) === 'undefined' ? '1024x1024' : String(process.env.OPENAI_API_IMAGE_SIZE);
+const OPENAI_API_IMAGE_RESPONSE_FORMAT = typeof (process.env.OPENAI_API_IMAGE_RESPONSE_FORMAT) === 'undefined' ? 'url' : String(process.env.OPENAI_API_IMAGE_RESPONSE_FORMAT);
+const OPENAI_API_USER = typeof (process.env.OPENAI_API_USER) === 'undefined' ? 'user' : String(process.env.OPENAI_API_USER);
+const OPENAI_API_TIMEOUT = typeof (process.env.OPENAI_API_TIMEOUT) === 'undefined' ? 30 : Number(process.env.OPENAI_API_TIMEOUT);
+
+
+const imageGenerator = async (message: string): Promise<any> => {
+
+    const response = await openai.createImage({
+        prompt: message,
+        n: 1,
+        size: (OPENAI_API_IMAGE_SIZE as CreateImageRequestSizeEnum),
+        response_format: (OPENAI_API_IMAGE_RESPONSE_FORMAT as CreateImageRequestResponseFormatEnum),
+        user: (OPENAI_API_USER as ChatCompletionRequestMessageRoleEnum)
+    },
+        { timeout: OPENAI_API_TIMEOUT * 1000 }
+    )
+
+
+    const result = String(response.data.data[0].url);
+    return result;
+
+}
+export default imageGenerator;
